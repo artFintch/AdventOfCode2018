812d40098aa8721991cf31d17562da16331f124a
Vyacheslav Khorkov <v.khorkov@tinkoff.ru>
2018-12-02 00:19:07 +0500

    Add day1 Python.

---
+++ Day1/Python/day1+.py
@@ -0,0 +1,13 @@
+import itertools
+
+freq = 0
+seen = {0}
+numbers = [int(line) for line in iter(input, '')]
+for num in itertools.cycle(numbers):
+    freq += num
+    if freq in seen:
+        print(freq)
+        break
+    seen.add(freq)
+			    
+			    
---
+++ Day1/Python/day1.py
@@ -0,0 +1,2 @@
+numbers = [int(line) for line in iter(input, '')]
+print(sum(numbers))
