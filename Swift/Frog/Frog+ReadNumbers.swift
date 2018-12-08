12ac791b64a305a8d531b5a8f68b99320a8fc846
Vyacheslav Khorkov <artfintch@ya.ru>
2018-12-08 11:22:54 +0500

    Add readNumbers method to Frog.

---
+++ Swift/Frog/Frog+ReadNumbers.swift
@@ -0,0 +1,19 @@
+//
+//  Frog+ReadNumbers.swift
+//  Swift
+//
+//  Created by Vyacheslav Khorkov on 08/12/2018.
+//  Copyright © 2018 Vyacheslav Khorkov. All rights reserved.
+//
+
+import Foundation
+
+public extension Frog {
+    
+    func readNumbers(_ separator: String = " ") -> [Int] {
+        guard let line = readLine() else { return [] }
+        return line
+            .components(separatedBy: separator)
+            .compactMap(Int.init)
+    }
+}
