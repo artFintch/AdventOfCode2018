dc2dc74b4040592d9c0dcb4b3f651fd37926a269
Vyacheslav Khorkov <artfintch@ya.ru>
2018-12-22 01:10:38 +0500

    Add day19 Swift.

--- Swift/Calendar/Day19/main.swift
+++ Swift/Calendar/Day19/main.swift
@@ -33,7 +33,6 @@ let operations: [String: Operation] = [
     "eqrr": .init("eqrr") { $0[$3] = try $0.get($1) == $0.get($2) ? 1 : 0 }
 ]
 
-
 func readInput() -> (Int, [(String, Instruction)]) {
     let input = Frog("input.txt")!
     let pointer = Int(input.readLine()!.components(separatedBy: " ")[1])!
@@ -48,34 +47,61 @@ func readInput() -> (Int, [(String, Instruction)]) {
     return (pointer, instructions)
 }
 
-func run(_ registers: [Int]) {
+func run(_ registers: [Int], debugMode: Bool = false) -> Int {
     var (bound, instructions) = readInput()
     var registers = registers
-    
-    var pointer = 0
+    var pointer = registers[bound]
     var step = 0
-    var pointers = [0]
-    var patterns: [Int: Int] = [:]
     while instructions.indices ~= pointer {
         registers[bound] = pointer
         let instruction = instructions[pointer]
         let operation = operations[instruction.0]!
-        let new = try! registers.applied(operation, instruction.1)
-        if new == registers {
-//            print(pointer, ":", instruction)
+        
+        if debugMode {
+            print(registers.map(String.init).joined(separator: ", "))
+            print(instruction.0, instruction.1.a, instruction.1.b, instruction.1.c)
         }
-        registers = new
+        
+        try? registers.apply(operation, instruction.1)
         pointer = registers[bound]
         pointer += 1
-//        print(step)
         step += 1
-        pointers.append(pointer)
-        if step % 1 == 0 {
+        
+        if debugMode {
             print(registers.map(String.init).joined(separator: ", "))
+            print(pointer)
+            print("")
         }
     }
-    print(registers)
+    return registers[0]
 }
 
-//measure(run([0, 0, 0, 0, 0, 0]))
-//measure(run([1, 0, 0, 0, 0, 0]))
+// Silver
+measure(run([0, 0, 0, 0, 0, 0]) == 1440)
+
+// Gold
+//measure(run([1, 0, 0, 0, 0, 0], debugMode: true))
+
+//  2 | seti 1 2 3 | r[3] = 1            |
+//  3 | mulr 5 3 2 | r[2] = r[5] * r[3]  |
+//  4 | eqrr 2 4 2 | r[2] = r[2] == r[4] | 10551358 % divider
+//  5 | addr 2 1 1 | r[1] += r[2]        | goto 7
+//  6 | addi 1 1 1 | r[1] += 1           | goto 8
+//  7 | addr 5 0 0 | r[0] += r[5]        |
+//  8 | addi 3 1 3 | r[3] += 1           |
+//  9 | gtrr 3 4 2 | r[2] = r[3] > r[4]  |
+// 10 | addr 1 2 1 | r[1] += r[2]        | goto 12
+// 11 | seti 2 6 1 | r[1] = 2            | goto 3
+// 12 | addi 5 1 5 | r[5] += 1           |
+// 13 | gtrr 5 4 2 | r[2] = r[5] > r[4]  | [..., 10551359, 10551358]
+// 14 | addr 2 1 1 | r[1] += r[2]        | goto 16
+// 15 | seti 1 8 1 | r[1] = 1            | goto 2
+// 16 | mulr 1 1 1 | r[1] *= r[1]        | => 16*16=256 => end
+
+var sum = 10551358
+for divider in 1..<10551358 {
+    if 10551358 % divider == 0 {
+        sum += divider
+    }
+}
+print(sum)
